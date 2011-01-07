2319ba64d4a40348efb1abe62cb3344ef889baa2
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-01-07 02:25:56 +0100

    require the adapters so they are loaded

--- lib/show_me_the_cookies.rb
+++ lib/show_me_the_cookies.rb
@@ -1,5 +1,21 @@
 module ShowMeTheCookies
-  @@session_cookie_name = nil
+  
+  require 'show_me_the_cookies/culerity'
+  require 'show_me_the_cookies/rack_test'
+  require 'show_me_the_cookies/selenium'
+
+  def current_driver_adapter
+    case (current_driver = Capybara.current_session.driver)
+    when Capybara::Driver::Selenium
+      ShowMeTheCookies::Selenium.new(current_driver.browser)
+    when Capybara::Driver::RackTest
+      ShowMeTheCookies::RackTest.new(current_driver)
+    when Capybara::Driver::Culerity
+      ShowMeTheCookies::Culerity.new(current_driver.browser)
+    else
+      unsupported_driver_spam
+    end
+  end
 
   def self.session_cookie_name=(name)
     @@session_cookie_name = name
@@ -17,6 +33,8 @@ module ShowMeTheCookies
   end
 
 private
+  @@session_cookie_name = nil
+  
   def self.unsupported_browser_spam
     raise "unsupported driver, use rack::test, selenium/webdriver or culerity"
   end
@@ -29,20 +47,6 @@ private
       @@session_cookie_name || Rails.application.config.session_options[:key]
     rescue NoMethodError => e
       raise "set ShowMeTheCookies.session_cookie_name= for your rails 2 app"
-    end
-  end
-
-
-  def current_driver_adapter
-    case (current_driver = Capybara.current_session.driver)
-    when Capybara::Driver::Selenium
-      ShowMeTheCookies::Selenium.new(current_driver.browser)
-    when Capybara::Driver::RackTest
-      ShowMeTheCookies::RackTest.new(current_driver)
-    when Capybara::Driver::Culerity
-      ShowMeTheCookies::Culerity.new(current_driver.browser)
-    else
-      unsupported_driver_spam
     end
   end
 end
