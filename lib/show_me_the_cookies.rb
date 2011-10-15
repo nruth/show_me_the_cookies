572e014061ef789980a62bbb37dcdf8e2d3af0dc
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-10-15 22:54:20 +0200

    moves driver selector to private

--- lib/show_me_the_cookies.rb
+++ lib/show_me_the_cookies.rb
@@ -2,18 +2,6 @@ module ShowMeTheCookies
   require 'show_me_the_cookies/rack_test'
   require 'show_me_the_cookies/selenium'
 
-  def current_driver_adapter
-    driver = Capybara.current_session.driver
-    case Capybara.current_driver
-    when :selenium
-      ShowMeTheCookies::Selenium.new driver
-    when :rack_test
-      ShowMeTheCookies::RackTest.new driver
-    else
-      raise "unsupported driver, use rack::test, selenium/webdriver or culerity"
-    end
-  end
-
   def inspect_cookies
     current_driver_adapter.show_me_the_cookies
   end
@@ -33,4 +21,16 @@ module ShowMeTheCookies
 
 private
   @@session_cookie_name = nil
+  
+  def current_driver_adapter
+    driver = Capybara.current_session.driver
+    case Capybara.current_driver
+    when :selenium
+      ShowMeTheCookies::Selenium.new driver
+    when :rack_test
+      ShowMeTheCookies::RackTest.new driver
+    else
+      raise "unsupported driver, use rack::test, selenium/webdriver or culerity"
+    end
+  end
 end
