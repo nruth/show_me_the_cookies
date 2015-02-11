8b0c245dd5c7c24ae1e4e6f176b4779418493eb9
Nicholas Rutherford <nick.rutherford@gmail.com>
2015-02-11 04:23:00 +0000

    minor version 2.6.0 add rack-test cookie support for reading httponly value

--- lib/show_me_the_cookies/version.rb
+++ lib/show_me_the_cookies/version.rb
@@ -1,3 +1,5 @@
+# change gem version here then use bundler's rake tasks to make a new release
+# try to use semantic versioning http://guides.rubygems.org/patterns/#semantic-versioning
 module ShowMeTheCookies
-  VERSION = "2.5.0"
+  VERSION = '2.6.0'
 end
